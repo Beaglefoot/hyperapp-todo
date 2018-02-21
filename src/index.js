ab1a0da4110dd0f74985ba584f598bab3bddba14
Stanislav Chernov <s.chernov@life-stream.tv>
2018-02-21 14:40:11 +0300

    Clean up

--- src/index.js
+++ src/index.js
@@ -6,16 +6,4 @@ import actions from 'src/actions';
 
 import 'src/styles/global.scss';
 
-const syncWithLocalStorage = view => (state, actions) => {
-  localStorage.setItem('hyperappTodoList', JSON.stringify(state));
-  return view(state, actions);
-};
-
-const savedState = JSON.parse(localStorage.getItem('hyperappTodoList'));
-
-app(
-  savedState || initialState,
-  actions,
-  syncWithLocalStorage(view),
-  document.getElementById('app')
-);
+app(initialState, actions, view, document.getElementById('app'));
